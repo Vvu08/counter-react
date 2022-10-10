66093a953d910a518dacfba1906fff9ef28d48c8
vvu <vasylechko.vlasta@student.uzhnu.edu.ua>
2022-10-10 23:11:49 +0300

    commit app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const Increment = () => {
+    setCount( count + 1)
+  }
+
+  const Decrement = () => {
+    setCount( count - 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container d-flex justify-content-center align-items-center flex-column'>
+      <h2 className='display-4'>Counter</h2>
+      <h1 className='fw-bold display-1 m-3 mt-1'>{count}</h1>
+      <div className='d-flex justify-content-center align-items-center'>
+        <button onClick={Increment} className='fw-bold m-1 btn btn-outline-primary shadow-sm'>+</button>
+        <button onClick={Decrement} className='fw-bold m-1 btn btn-outline-primary shadow-sm'>-</button>
+      </div>
     </div>
   );
 }
